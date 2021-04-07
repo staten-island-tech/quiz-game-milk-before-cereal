f87945475472c768dd2f8dbd0f7a4d58a870b172
JasonC123456789 <71746733+JasonC123456789@users.noreply.github.com>
2021-04-07 00:02:17 -0400

    (╯°□°）╯︵ ┻━┻  4

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ console.log("connected");
 const startButton = document.getElementById('start-btn')
 
 startButton.addEventListener('click', startGame) 
+
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
