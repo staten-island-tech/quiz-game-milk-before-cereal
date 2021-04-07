f2d0159375667bc69ce95d69897881ad5c6a47e1
JasonC123456789 <71746733+JasonC123456789@users.noreply.github.com>
2021-04-06 23:59:56 -0400

    (╯°□°）╯︵ ┻━┻

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 console.log("connected");
+
+//Start Quiz//
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame) 
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+}
+
+//Insert Question//
+
+//Submit Answer//
+
+//If Correct Answer is submitted, display correct modal
+//Else display incorrect modal
+
+//If question # < questions.length display next question
+//else display results
