04b2bc00c70bdb1f8ea2c3c26d4cc40e0c7d29fb
kyle22313 <71525932+kyle22313@users.noreply.github.com>
2021-04-11 22:00:49 -0400

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,33 @@
 console.log("connected");
 
 //Start Quiz//
-const startButton = document.getElementById('start-btn')
-
-startButton.addEventListener('click', startGame) 
+const startButton = document.getElementById("start-btn");
+const questionContainElement = document.getElementById("question");
+startButton.addEventListener("click", startGame);
 
 function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
+  console.log("Started");
+  startButton.classList.add("hide");
+  questionContainElement.classList.remove("hide");
+  setnextquestion;
 }
 
 //Insert Question//
-
+const questions = [
+  {
+    question: "what is this pokemon",
+    answer: [
+      { Text: "pokemon1", correct: false },
+      { Text: "pokemon2", correct: true },
+    ],
+  },
+];
+function setnextquestion
 //Submit Answer//
 
 //If Correct Answer is submitted, display correct modal
 //Else display incorrect modal
 
 //If question # < questions.length display next question
+
 //else display results
