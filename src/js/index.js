d93e230b61193f505a9bc838415302d5e0cb1627
JasonC123456789 <71746733+JasonC123456789@users.noreply.github.com>
2021-04-28 11:24:18 -0400

    a

--- src/js/index.js
+++ src/js/index.js
@@ -173,8 +173,8 @@ const questions = [
   ];
   
   var image = document.getElementById('x');
-  var imageArray = ['image/two.jpg','image/one.jpg',
-  '/two.jpg','/three.1551a0c4.jpg','/four.0fd1ab7b.jpg','/five.4a723cfc.jpg',
+  var imageArray = ['image/front.jpg','image/one.jpg',
+  'image/two.c290eb48.jpg','/three.1551a0c4.jpg','/four.0fd1ab7b.jpg','/five.4a723cfc.jpg',
   '/six.e48d2430.jpg','/seven.70007d00.jpg','/eight.c10fb99a.png','/nine.d6673d48.jpg', ];
   var imageIndex = 1; 
 
